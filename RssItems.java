59130412bb6853724dd706a1e280d6e4741835ff
ovitan <it.hung.0805@gmail.com>
2014-09-28 11:46:09 +0700

    Create RssItems.java

---
+++ RssItems.java
@@ -0,0 +1,34 @@
+package com.models;
+
+public class RssItems {
+
+	private String title;
+	private String desciptions;
+	private String link;
+	private String date;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesciptions() {
+		return desciptions;
+	}
+	public void setDesciptions(String desciptions) {
+		this.desciptions = desciptions;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
